34a9718bac5295b99af48fdeac61719c3c8b2165
JeanKonan <kon22005@byui.edu>
2023-10-21 03:21:34 -0600

    hero img completed

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,13 @@
+const temp = 43;
+const windspeed = 5;
+const windchill = document.querySelector(".windchill");
+
+if (temp <= 50 & windspeed > 3) {
+    windchill.textContent = "Windchill: " + (35.74 + 0.6215 * temp - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temp *  Math.pow(windspeed, 0.16)).toFixed(3) + " mph";
+} else {
+    windchill.textContent = "Windchill: N/A";
+}
+
+
+
+
