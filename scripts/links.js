69f7297292646ef605f4283e85364dfd01a1d17c
JeanKonan <kon22005@byui.edu>
2023-11-09 21:34:43 -0700

    lesson 09 completed

---
+++ scripts/links.js
@@ -0,0 +1,34 @@
+const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+
+const section = document.querySelector(".card");
+const h3 = document.createElement("h3");
+const ul = document.createElement("ul");
+
+h3.textContent = "Learning Activities";
+
+section.appendChild(h3);
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+}
+
+const displayLinks = (data) => {
+    data.lessons.forEach(lesson => {
+        lesson.links.forEach(link => {
+            let li = document.createElement("li");
+            let a = document.createElement("a");
+
+            a.setAttribute("href", link.url);
+            a.setAttribute("target", "_blank");
+            a.textContent = `${lesson.lesson}: ${link.title}`;
+
+            li.appendChild(a);
+            ul.appendChild(li);
+        });
+    });
+    section.appendChild(ul);
+}
+
+getLinks();
