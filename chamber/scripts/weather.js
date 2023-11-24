3f6f0c4e3e71ce1a0768e4dd7f524b126d85ec16
JeanKonan <kon22005@byui.edu>
2023-11-23 20:46:11 -0700

    banner days changed

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -7,7 +7,7 @@ window.onload = function() {
     var dayOfWeek = d.getDay();
 
     // Check if it's Monday (1), Tuesday (2), or Wednesday (3)
-    if (dayOfWeek >= 1 && dayOfWeek <= 4) {
+    if (dayOfWeek >= 1 && dayOfWeek <= 3) {
         document.getElementById('banner').style.display = 'block';
     } else {
         document.getElementById('banner').style.display = 'none';   
